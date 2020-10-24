284d66b5bd5704edb2be5be665a17e2c5b77f375
Aanika-Sadana <as3782@njit.edu>
2020-10-24 10:42:41 +0000

    added files and directories for profile and username hw

---
+++ project/home.php
@@ -0,0 +1,16 @@
+<?php
+require_once(__DIR__ . "/partials/nav.php");
+?>
+
+<?php
+
+//use this to safely get the email to display
+$email = "";
+
+if(isset($_SESSION["user"]) && isset($_SESSION["user"]["email"])){
+	$email = $_SESSION["user"]["email"];
+}
+
+?>
+
+<p>Welcome, <?php echo $email; ?> </p>
---
+++ project/partials/README.md
@@ -0,0 +1,5 @@
+contains files taht aren't standalone
+
+should never be called in the URL by themselves
+
+meant to be **include**d or **require**d into other files
